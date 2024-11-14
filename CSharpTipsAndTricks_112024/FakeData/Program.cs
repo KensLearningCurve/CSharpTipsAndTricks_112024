442331217aba9b1f88ab6036c5ffe8f47a807c31
kenji.elzerman <kenslearningcurve@gmail.com>
2024-11-14 09:56:04 +0000

    Adding FakeData

---
+++ CSharpTipsAndTricks_112024/FakeData/Program.cs
@@ -0,0 +1,25 @@
+﻿using Logic;
+using Bogus;
+
+var products = new ProductGenerator().Generate(10);
+
+foreach (var product in products)
+{
+    Console.WriteLine($"The product {product.Title} has a stock of {product.Stock}");
+}
+
+
+class ProductGenerator: Faker<Product>
+{
+    public ProductGenerator()
+    {
+        var titles = new string[] { "Laptop", "Smartphone", "Tablet", "Headphones", "Monitor" };
+
+        UseSeed(10)
+            .RuleFor(x => x.Title, f => f.PickRandom(titles))
+            .RuleFor(x => x.Stock, f => f.Random.Int(0, 100))
+            .RuleFor(x => x.Available, f => f.Random.Bool())
+            .RuleFor(x => x.Status, f => f.PickRandom<Status>())
+            .RuleFor(x => x.Id, f => f.Random.Int());
+    }
+}
