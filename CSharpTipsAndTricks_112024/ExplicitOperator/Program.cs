b37f4388c533b17e12c944bd5221b763c8d427ea
kenji.elzerman <kenslearningcurve@gmail.com>
2024-11-14 10:22:25 +0000

    Adding ExplicitOperator

---
+++ CSharpTipsAndTricks_112024/ExplicitOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Logic;
+
+Product sugar = ProductList.Products.Single(x => x.Title == "Sugar");
+DiscontinuedProduct discontinuedSugar = (DiscontinuedProduct)sugar;
+
+public class DiscontinuedProduct
+{
+    public string Title { get; set; }
+    public int Id { get; set; }
+
+
+    public static explicit operator DiscontinuedProduct(Product product)
+    {
+        return new()
+        {
+            Id = product.Id,
+            Title = product.Title,
+        };
+    }
+}
