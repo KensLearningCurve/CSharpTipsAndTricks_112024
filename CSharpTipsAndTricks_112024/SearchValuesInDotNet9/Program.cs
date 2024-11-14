0a7cda7f279118c25dbd77e97d0e837a85cf7e6f
kenji.elzerman <kenslearningcurve@gmail.com>
2024-11-14 10:34:56 +0000

    Adding the SearchValues

---
+++ CSharpTipsAndTricks_112024/SearchValuesInDotNet9/Program.cs
@@ -0,0 +1,10 @@
+﻿using Logic;
+using System.Buffers;
+
+
+string[] productTitles = ProductList.Products.Select(x => x.Title).ToArray();
+
+SearchValues<string> svProducts = SearchValues.Create(productTitles, StringComparison.OrdinalIgnoreCase);
+
+IEnumerable<Product> found = ProductList.Products
+    .Where(x => svProducts.Contains(x.Title));
