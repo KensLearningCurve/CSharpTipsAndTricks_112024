b8966c033be6285159f122ba50568b7d10670ab2
kenji.elzerman <kenslearningcurve@gmail.com>
2024-11-14 08:48:00 +0000

    Adding Sql and LINQ with EF

---
+++ CSharpTipsAndTricks_112024/SQLAndLINQwithEF/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using Logic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SQLAndLINQwithEF;
+
+public class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+}
---
+++ CSharpTipsAndTricks_112024/SQLAndLINQwithEF/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLAndLINQwithEF;
+
+var options = new DbContextOptionsBuilder<ProductContext>()
+           .UseInMemoryDatabase(databaseName: "InMemoryProductDatabase")
+           .Options;
+
+
+using (var context = new ProductContext(options))
+{
+    var products = context.Products
+            .FromSql($"SELECT * FROM Products WHERE Title like '%su%'")
+            .Where(x => x.Available)
+            .ToList();
+}
+
