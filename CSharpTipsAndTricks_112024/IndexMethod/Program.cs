ddf7bddb95a13236dcce11d002e75da2d477f0c4
kenji.elzerman <kenslearningcurve@gmail.com>
2024-11-14 08:24:09 +0000

    Adding IndexMethod

---
+++ CSharpTipsAndTricks_112024/IndexMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Logic;
+
+for (int i = 0; i<ProductList.Products.Count; i++)
+{
+    Console.WriteLine($"Index = {i}, Name = {ProductList.Products[i].Title}");
+}
+
+foreach ((int index, Product product) in ProductList.Products.Index())
+{
+    Console.WriteLine($"Index = {index}, Name = {product.Title}");
+}
